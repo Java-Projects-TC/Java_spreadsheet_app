30b2fa6db927ae85d664fd65306be9bf1c9ca2d1
Tiger Cross <tc2017@ic.ac.uk>
2018-02-28 21:07:05 +0000

    "Stage 3 Implementation almost completed."

--- src/spreadsheet/Cell.java
+++ src/spreadsheet/Cell.java
@@ -1,14 +1,20 @@
 package spreadsheet;
 
 import common.api.CellLocation;
+import common.api.ExpressionUtils;
+import common.api.monitor.Tracker;
 import common.api.value.Value;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Cell {
+public class Cell implements Tracker<Cell>{
 
   private String expression;
   private Value value;
   private final CellLocation location;
   private Spreadsheet spreadsheet;
+  private Set<Tracker<Cell>> trackedCells;
+  private Set<Tracker<Cell>> trackedBy;
 
   Cell(CellLocation location, Spreadsheet spreadsheet){
     this("", location, spreadsheet);
@@ -19,6 +25,8 @@ public class Cell {
     this.value = null;
     this.location = location;
     this.spreadsheet = spreadsheet;
+    this.trackedCells = new HashSet<>();
+
   }
 
   public String getExpression() {
@@ -35,5 +43,38 @@ public class Cell {
 
   public void setValue(Value value) {
     this.value = value;
+  }
+
+  public CellLocation getLocation() {
+    return location;
+  }
+
+  @Override
+  public void update(Cell changed) {
+
+    // remove cells it was depending upon from tracker
+    trackedCells.forEach(t -> changed.removeTracker(t));
+
+    // change cells expression to new one
+    this.setExpression(changed.getExpression());
+
+    // extract cell locations and convert to cells
+
+    // NOTE: can you use streams here? attempt later.
+    Set<CellLocation> refCellLocations = ExpressionUtils.getReferencedLocations
+        (changed.getExpression());
+    for (CellLocation refCellLocation : refCellLocations) {
+      trackedCells.add(spreadsheet.getCell(refCellLocation));
+    }
+
+    // Inform all Tracker<Cell> subscribed that expression has changed
+    for (Tracker<Cell> tracker : trackedBy) {
+      tracker.update(this);
+      spreadsheet.addToCellsToCompute((Cell) tracker); // Had to cast here??
+    }
+  }
+
+  private void removeTracker(Tracker<Cell> tracker) {
+    trackedCells.remove(tracker);
   }
 }
--- src/spreadsheet/Spreadsheet.java
+++ src/spreadsheet/Spreadsheet.java
@@ -4,35 +4,51 @@ import common.api.CellLocation;
 import common.api.Tabular;
 import common.api.value.StringValue;
 import common.api.value.Value;
+import java.util.ArrayDeque;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
 
 public class Spreadsheet implements Tabular {
 
   private Map<CellLocation, Cell> cells = new HashMap<>();
+  private Queue<Cell> cellsToCompute = new ArrayDeque<>();
+
+  Cell getCell(CellLocation location) {
+    return cells.getOrDefault(location, new Cell(location, this));
+  }
+
+  void addToCellsToCompute(Cell cell) {
+    cellsToCompute.add(cell);
+  }
 
   @Override
   public void setExpression(CellLocation location, String expression) {
     if (!cells.containsKey(location)) {
       cells.put(location, new Cell(expression, location, this));
+    } else {
+      cells.get(location).setExpression(expression);
     }
-    cells.get(location).setExpression(expression);
     cells.get(location).setValue(new StringValue(expression));
   }
 
   @Override
   public String getExpression(CellLocation location) {
-      return cells.getOrDefault(location, new Cell(location, this))
-          .getExpression();
+    return cells.containsKey(location) ? cells.get(location).getExpression()
+        : "";
   }
 
   @Override
   public Value getValue(CellLocation location) {
-    return cells.getOrDefault(location, new Cell(location, this)).getValue();
+    return cells.containsKey(location) ? cells.get(location).getValue() : null;
   }
 
   @Override
   public void recompute() {
-
+    for (int i = 0; i < cellsToCompute.size(); i++) {
+      Cell cell = cellsToCompute.poll();
+      cell.setValue(new StringValue(cell.getExpression()));
+    }
   }
+
 }
